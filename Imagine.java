3b39fb9112d8f901bed3575bd342e958dd32c391
TothCsilla98 <49196736+TothCsilla98@users.noreply.github.com>
2019-10-28 20:16:55 +0200

    Update Imagine.java
    modificat

--- Imagine.java
+++ Imagine.java
@@ -1,14 +1,22 @@
 package dp_lab2;
 
+import java.util.concurrent.TimeUnit;
+
 public class Imagine implements Element{
-	private String nume;
+	public String nume;
 
 	public Imagine(String nume) {
 		super();
 		this.nume = nume;
+		//imageName = nume;
+		try {
+			TimeUnit.SECONDS.sleep(5);
+			} catch (InterruptedException e) {
+			e.printStackTrace();
+			}
 	}
 	
-
+	@Override
 	public void print()
 	{
 		System.out.println("Imagine: " + this.nume);
