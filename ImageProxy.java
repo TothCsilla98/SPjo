a3aec726b2c69944f39e89981568d6345a30d0a2
TothCsilla98 <49196736+TothCsilla98@users.noreply.github.com>
2019-11-12 11:06:21 +0200

    Update ImageProxy.java

--- ImageProxy.java
+++ ImageProxy.java
@@ -1,26 +1,44 @@
 package dp_lab2;
 
-public class ImageProxy implements  Element {
+import java.util.concurrent.TimeUnit;
 
+public class ImageProxy implements Element {
+	private String nume;
+	private Imagine realImage;
 
-private String nume;
-private Imagine realImage;
-public ImageProxy(String nume) {
-	super();
-	this.nume = nume;
-}
-
-
-public void print()
-{
-
-	if(realImage == null ) 
-		realImage=new Imagine(this.nume);
-	else
+	public ImageProxy(String nume) {
+		super();
+		this.nume =nume;
+		
+		
+	}	
+	public void print()
+	{  
+  
+	if(realImage==null )	
+	  realImage=new Imagine(this.nume);
 		realImage.print();
 	
+	}
 	
-	
+public void accept(Visitor a) {
+		a.visit(this);
+	}
+
+	@Override
+  public void add(Element element) {
+    System.out.println("ERORR - IMG PROXY");
+  }
+
+  @Override
+  public void remove(Element element) {
+    System.out.println("ERORR - IMG PROXY");
+  }
+
+  @Override
+  public Element getElement(int index) {
+    System.out.println("ERORR - IMG PROXY");
+    return null;
+  }
 }
 
-}
