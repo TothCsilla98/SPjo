e5a8a723c2b0deb54d8eeb654bfcb4beff840311
TothCsilla98 <49196736+TothCsilla98@users.noreply.github.com>
2019-11-12 11:09:37 +0200

    Update Tabel.java

--- Tabel.java
+++ Tabel.java
@@ -1,14 +1,42 @@
 package dp_lab2;
 
+import java.util.List;
+
 public class Tabel implements Element{
 	private String nume;
+
 
 	public Tabel(String nume) {
 		super();
 		this.nume = nume;
 	}
+	
+	@Override
 	public void print()
 	{
-		System.out.println("Tabel:  " + this.nume);
+		System.out.println("Tabel: " + this.nume);
 	}
+	
+	public void accept(Visitor a) {
+		a.visit(this);  
+	
+
+		
+	}
+
+	public void add(Element x) {
+    System.out.println("ERORR");
+  }
+
+  @Override
+  public void remove(Element x) {
+    System.out.println("ERORR");
+  }
+
+  @Override
+  public Element getElement(int index) {
+    System.out.println("ERORR");
+    return null;
+  }
 }
+
