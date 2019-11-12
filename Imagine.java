7492cf3626f60c19ac24dd4998b5cad7346903b5
TothCsilla98 <49196736+TothCsilla98@users.noreply.github.com>
2019-11-12 11:07:22 +0200

    Update Imagine.java

--- Imagine.java
+++ Imagine.java
@@ -3,12 +3,11 @@ package dp_lab2;
 import java.util.concurrent.TimeUnit;
 
 public class Imagine implements Element{
-	public String nume;
+	String nume;
 
 	public Imagine(String nume) {
 		super();
 		this.nume = nume;
-		//imageName = nume;
 		try {
 			TimeUnit.SECONDS.sleep(5);
 			} catch (InterruptedException e) {
@@ -21,4 +20,24 @@ public class Imagine implements Element{
 	{
 		System.out.println("Imagine: " + this.nume);
 	}
+	
+	public void accept(Visitor a) {
+		a.visit(this);
+			
+	}
+	 public void add(Element element) {
+    System.out.println("ERORR");
+  }
+
+  @Override
+  public void remove(Element element) {
+    System.out.println("ERORR");
+  }
+
+  @Override
+  public Element getElement(int index) {
+    System.out.println("ERORR");
+    return null;
+  }
 }
+
