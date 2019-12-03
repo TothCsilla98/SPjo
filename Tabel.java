7f508411d0614e61f3255c0e80eda9c9494216ad
TothCsilla98 <49196736+TothCsilla98@users.noreply.github.com>
2019-12-03 22:04:04 +0200

    Update Tabel.java

--- Tabel.java
+++ Tabel.java
@@ -1,9 +1,12 @@
 package dp_lab2;
 
 import java.util.List;
+import java.util.Observer;
 
-public class Tabel implements Element{
+public class Tabel implements Element,Observable{
 	private String nume;
+	String oldValue;
+	private List<Observerr> obs;
 
 
 	public Tabel(String nume) {
@@ -38,5 +41,36 @@ public class Tabel implements Element{
     System.out.println("ERORR");
     return null;
   }
+
+@Override
+public void addObserver(Observerr obs) {
+	this.obs.add(obs);
+	
 }
 
+@Override
+public void removeObserver(Observerr obs) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void notifyObservers() {
+	
+	for(Observerr o :obs)
+	{
+		o.update(oldValue,nume);
+	}
+	
+}
+
+@Override
+public void setNewValue(String newValue) {
+	oldValue=this.nume;
+    nume=newValue;
+    Observer obs =null;;
+
+	this.notifyObservers();
+	
+}
+}
